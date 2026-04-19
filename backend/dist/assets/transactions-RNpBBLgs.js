94c92e671d0069de690777937181297bc09529bc
zotrust <zotrustdapp@gmail.com>
2026-04-19 07:51:15 +0000

    dfssfds

---
+++ backend/dist/assets/transactions-RNpBBLgs.js
@@ -0,0 +1,16 @@
+import{I as f,F as a,G as m}from"./index-BFI8d_nw.js";import{c as p}from"./index-CCwJfBJe.js";import"./index-ul5z2hWP.js";import"./index-C7vADx--.js";import"./if-defined-BZ2G6Ztu.js";import"./index-Dfw7VDAl.js";import"./index-KE4SyB4T.js";import"./index-C89zYsPB.js";const d=f`
+  :host > wui-flex:first-child {
+    height: 500px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    scrollbar-width: none;
+  }
+
+  :host > wui-flex:first-child::-webkit-scrollbar {
+    display: none;
+  }
+`;var u=function(o,t,i,r){var n=arguments.length,e=n<3?t:r===null?r=Object.getOwnPropertyDescriptor(t,i):r,l;if(typeof Reflect=="object"&&typeof Reflect.decorate=="function")e=Reflect.decorate(o,t,i,r);else for(var s=o.length-1;s>=0;s--)(l=o[s])&&(e=(n<3?l(e):n>3?l(t,i,e):l(t,i))||e);return n>3&&e&&Object.defineProperty(t,i,e),e};let c=class extends a{render(){return m`
+      <wui-flex flexDirection="column" .padding=${["0","3","3","3"]} gap="3">
+        <w3m-activity-list page="activity"></w3m-activity-list>
+      </wui-flex>
+    `}};c.styles=d;c=u([p("w3m-transactions-view")],c);export{c as W3mTransactionsView};
